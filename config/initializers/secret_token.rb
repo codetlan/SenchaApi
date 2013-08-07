87d87a368d7e899f682a0642f0792f55b1f234a4
jose armando gonzalez lopez <iam@armando.mx>
2013-08-07 01:42:05 -0500

    se creo el login con devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SenchaApi::Application.config.secret_token = 'fde53c8bc35428b090754f12a2c36e9b54966239e9e42584f31a0789b7c62127f8091e1b74606f8ff91457716637b8f5362044dbfd49828b0056fc5d8e217324'
